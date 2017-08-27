b87b52c9d1b08cd4c92003d5c82726205843bc08
Benjamin Bressler <bressleb@gmail.com>
2017-08-26 17:40:20 -0700

    add styles and readme specifications

--- readME.md.txt
+++ readME.md.txt
@@ -9,5 +9,11 @@ Open repository index in web browser
 Open webpage
 Put any numerical value in the form
 Press submit button to display to arrays, one of odd numbers and one of even.
+Specifications:
+My program, will process the userinput, when the user presses the submit button.
+The numerical value put in by the user will then be put into an if function to determine if it is odd or even.
+This is done by the modolu, the modulo is used to determine if the number is evenly divisible by two, therefore making it
+an even number. In the event it is not divisible by two, my program will determine it is an odd number. The odd & even numbers
+are then appended to odds & evens lists. 
 Copyright (c) 2017 Benjamim Bressler Copyright Holder All Rights Reserved.
 This software is licensed under the MIT license.
--- scripts/script2.js
+++ scripts/script2.js
@@ -1,28 +1,30 @@
 // front end logic
 $(function() {
   $("form").submit(function() {
+       $("#info").show();
+       $("#info2").show();
       event.preventDefault();
-      var num=$(this.answer).val();
-      var currentnumber=1;
-      while(currentnumber<=num) {
-        var newentry= $("<li>"+currentnumber+"</li>");
-        var evenslist= $("#even");
-        var oddslist= $("#odds");
-        if(iseven(currentnumber)) {
-         newentry.appendTo(evenslist);
-        }else{
-         newentry.appendTo(oddslist);
+      var num = $(this.answer).val();
+      var currentnumber = 1;
+      while(currentnumber <= num) {
+        var newentry = $("<li>" + currentnumber + "</li>");
+        var evenslist = $("#even");
+        var oddslist = $("#odds");
+        if (iseven(currentnumber)) {
+          newentry.appendTo(evenslist);
+        } else {
+          newentry.appendTo(oddslist);
         }
 
-        currentnumber++
+        ++currentnumber;
       }
   });
 });
     // back end logic
 function iseven(number) {
-  if(number%2==0) {
-   return true;
-  }else{
-   return false;
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
   }
 };
